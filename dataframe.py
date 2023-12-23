0f34e807ed31edb7a2a939cdbab451470c76812b
Seb65789 <nati.seb.2002@gmail.com>
2023-12-23 17:49:19 +0100

    Aucune modification

--- dataframe.py
+++ dataframe.py
@@ -3,16 +3,17 @@ import numpy as np
 
 # Fonctions EAR et MAR
 def eye_aspect_ratio(eye):
-    A = np.linalg.norm(np.array(eval(eye[1])) - np.array(eval(eye[5])))
-    B = np.linalg.norm(np.array(eval(eye[2])) - np.array(eval(eye[4])))
-    C = np.linalg.norm(np.array(eval(eye[0])) - np.array(eval(eye[3])))
+    A = np.linalg.norm(np.array(eval(eye.iloc[1])) - np.array(eval(eye.iloc[5])))
+    B = np.linalg.norm(np.array(eval(eye.iloc[2])) - np.array(eval(eye.iloc[4])))
+    C = np.linalg.norm(np.array(eval(eye.iloc[0])) - np.array(eval(eye.iloc[3])))
     ear = (A + B) / (2.0 * C)
+    
     return ear
 
 def mouth_aspect_ratio(mouth):
-    A = np.linalg.norm(np.array(eval(mouth[1])) - np.array(eval(mouth[5])))
-    B = np.linalg.norm(np.array(eval(mouth[2])) - np.array(eval(mouth[4])))
-    C = np.linalg.norm(np.array(eval(mouth[0])) - np.array(eval(mouth[3])))
+    A = np.linalg.norm(np.array(eval(mouth.iloc[1])) - np.array(eval(mouth.iloc[5])))
+    B = np.linalg.norm(np.array(eval(mouth.iloc[2])) - np.array(eval(mouth.iloc[4])))
+    C = np.linalg.norm(np.array(eval(mouth.iloc[0])) - np.array(eval(mouth.iloc[3])))
     mar = (A + B) / (2.0 * C)
     return mar
 
@@ -33,8 +34,6 @@ for video_file in video_files:
     # Charger le CSV
     df = pd.read_csv(video_file)
     taille_premiere_colonne = len(df.iloc[:, 0])
-
-
 
 for frame in range(taille_premiere_colonne):  # Supposant 876 frames par vidéo
     for signe in signes:
@@ -57,8 +56,8 @@ for video_name in video_names:
 
     # Calculer l'EAR pour chaque œil et ajouter les valeurs à df_final
     for frame in range(len(df)):
-        eye_coordinates_left = df.loc[frame, ['left_eye_463', 'left_eye_385', 'left_eye_387', 'left_eye_263', 'left_eye_373', 'left_eye_380']]
-        eye_coordinates_right = df.loc[frame, ['right_eye_133','right_eye_158','right_eye_160','right_eye_33','right_eye_144','right_eye_153']]
+        eye_coordinates_left = df.loc[frame, ['left_eye_263','left_eye_387','left_eye_385','left_eye_326','left_eye_380','left_eye_373']]
+        eye_coordinates_right = df.loc[frame, ['right_eye_33','right_eye_160','right_eye_158','right_eye_133','right_eye_153','right_eye_144']]
         mouth_coordinates = df.loc[frame, ['mouth_78','mouth_82','mouth_312', 'mouth_308','mouth_317','mouth_87']]
         
         EAR_left = eye_aspect_ratio(eye_coordinates_left)
