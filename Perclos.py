f416ccf0652a7e32db55e36ae6a106f6f46d42d3
Seb65789 <nati.seb.2002@gmail.com>
2024-01-20 01:38:57 +0100

    Perclos.py renommer en perclos.py

--- Perclos.py
+++
@@ -1,4 +0,0 @@
-# La fonction prend donc en parmètre le nombre de frames totale et le nombre de frames passées les yeux fermés
-# Elle retourne donc un pourcentage PERCLOS
-def perclos(nb_frame_tot,nb_frame_ferme) :
-    return (nb_frame_ferme/nb_frame_tot) * 100
---
+++ perclos.py
@@ -0,0 +1,4 @@
+# Fonctoin qui calcule le PERCLOS
+
+def perclos(nb_ferme,nb_tot) :
+    return (nb_ferme/nb_tot)*100
